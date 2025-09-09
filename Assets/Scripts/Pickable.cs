3ae490bc5b19ca35f636c236b71d66d70035b94e
HyGigs <giggs.samestagiga+github@gmail.com>
2025-09-09 20:51:49 +0700

    feat: Pickable Objects

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    [SerializeField] private PickableType _pickableType;
+
+    public Action<Pickable> OnPicked;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnPicked?.Invoke(this);
+        }
+    }
+}
